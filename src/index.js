51ec5c24255a5a7399fd582dc6391953bd609063
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-02-04 19:15:37 +0100

    fix: correct import statement

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import ApplicationLetter from './ApplicationLetter';
+import { ApplicationLetter } from './ApplicationLetter';
 
 ReactDOM.render(
   <ApplicationLetter />,
