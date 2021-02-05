5d6d487f435496565dc02101f38ab682928f2b43
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-02-05 15:34:45 +0100

    feat: install dependencies for running the build

---
+++ webpack.prod.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.js'),
+  mode: 'production',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'build'),
+    publicPath: './',
+  },
+  optimization: {
+    minimizer: [new OptimizeCssAssetsPlugin(), new TerserPlugin()],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({ filename: 'applicationLetter.css' }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(jpg|gif)$/i,
+        use: {
+          loader: 'file-loader',
+          options: {
+            esModule: false,
+            name: '[name].[hash].[ext]',
+            outputPath: 'assets/images',
+          },
+        },
+      },
+    ],
+  },
+};
