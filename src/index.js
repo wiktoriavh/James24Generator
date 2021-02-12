88cb5cdd89f691c8b548bab97f1a27d6917ad0b2
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-02-12 17:43:27 +0100

    fix: correct pathing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { ApplicationLetter } from './ApplicationLetter';
+import { ApplicationLetter } from './application/ApplicationLetter';
 
 ReactDOM.render(
   <ApplicationLetter />,
